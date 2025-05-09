dccc0a5e4739765ee4f72352c2eb71e9c729cb7d
Pallavi Sharma <102752671+pallavi-analytics@users.noreply.github.com>
2025-05-09 18:11:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let score = 0;
+let current = 0;
+let answered = false;
+
 const questions = [
   {
     question: "What is the atomic number of Carbon?",
@@ -9,10 +13,25 @@ const questions = [
     options: ["Oxygen", "Helium", "Nitrogen", "Hydrogen"],
     correct: 1
   }
+  {
+  question: "Which of the following is the lightest subatomic particle?",
+  options: ["Proton", "Neutron", "Electron", "Nucleus"],
+  correct: 2
+},
+{
+  question: "What is the maximum number of electrons that can be in the second shell (L shell)?",
+  options: ["2", "4", "6", "8"],
+  correct: 3
+},
+{
+  question: "Who discovered the neutron?",
+  options: ["J.J. Thomson", "James Chadwick", "Ernest Rutherford", "Niels Bohr"],
+  correct: 1
+}
+
 ];
 
-let current = 0;
-let answered = false;
+
 
 function loadQuestion() {
   answered = false;
@@ -43,6 +62,7 @@ function showFeedback(selected, btn) {
   if (selected === correctIndex) {
     resultBox.innerText = "✅ Correct!";
     resultBox.style.color = "green";
+    score++;
   } else {
     resultBox.innerText = `❌ Wrong! Correct answer: ${questions[correctIndex]}`;
     resultBox.style.color = "red";
@@ -50,13 +70,40 @@ function showFeedback(selected, btn) {
 
   resultBox.classList.remove("hidden");
 }
+function showFinalScore() {
+  let message = "";
+  switch (score) {
+    case 5:
+      message = "🌟 Brilliant! You scored 5/5 🎉";
+      break;
+    case 4:
+      message = "👏 Well done! You scored 4/5";
+      break;
+    case 3:
+      message = "🙂 Hmm... okay! You scored 3/5";
+      break;
+    case 2:
+      message = "😐 You can do better! You scored 2/5";
+      break;
+    case 1:
+      message = "😕 Try again! You scored 1/5";
+      break;
+    default:
+      message = "😅 Oh c'mon! You scored 0/5";
+  }
+
+  document.getElementById("quiz-box").innerHTML = `
+    <h2>Quiz Completed!</h2>
+    <p>${message}</p>
+  `;
+}
 
 document.getElementById("next-btn").addEventListener("click", () => {
   current++;
   if (current < questions.length) {
     loadQuestion();
   } else {
-    document.getElementById("quiz-box").innerHTML = "<h3>🎉 Quiz Completed!</h3>";
+    showFinalScore();
   }
 });
 
